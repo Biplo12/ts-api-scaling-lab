5f6a57841c352dd80732610181cae7602c1ad6a1
Robert Siński <robertbiplosek@gmail.com>
2026-08-30 16:06:47 +0200

    chore: first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import Fastify from "fastify";
+
+const PORT = Number(process.env.PORT) || 3000;
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", (request, reply) => {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: PORT });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
